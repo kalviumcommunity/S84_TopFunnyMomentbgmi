f3d5fb4040f583d715facf38acd06fe30c89cec8
Sksham Kaushal <skshamkaushal@gmail.com>
2025-03-05 17:50:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const { connect } = require("mongoose");
+const connectToDb = require("./database/db");
+const RouteMoment = require('./routes/routes');
 const app = express();
-
-
-app.use(express.json())
+require('dotenv').config();
 const Port = process.env.PORT || 9090;
 const DB_url = process.env.DB_URL;
 
 
 app.get("/", (req, res) => {
-    res.send("My name is Sksham kaushal. I am from squad-84")
+    res.send("<h2> My name is SKSHAM. I am from squad-84 </h2>")
 })
 
-app.listen(3000, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.use('/', RouteMoment);
+app.listen(Port, async() => {
+    try{
+        await connectToDb(db_url);
+        console.log(`Server is running on port http://localhost:${Port}`);
+        console.log(`Successfull connected to database with url as ${db_url}`)
+    }
+    catch(err){
+        console.log(err)
+    }
 })
