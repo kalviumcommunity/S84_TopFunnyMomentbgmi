572ddec493aad153eb9602db7d72d5cfb7d15cc2
Sksham Kaushal <skshamkaushal@gmail.com>
2025-03-06 09:57:12 +0530

    landing page

---
+++ Frontend/src/LandingPage.jsx
@@ -0,0 +1,15 @@
+
+function LandingPage(){
+    return(
+        <>
+            <div>
+                <h1>This is my ASAP landing page
+                    <br/>
+                    Hello my name is sksham Nice to meet you.
+                    <h3>Here you can find the top silly moment happened in BGMI</h3>  
+                </h1>
+            </div>
+        </>
+    )
+}
+export default LandingPage
